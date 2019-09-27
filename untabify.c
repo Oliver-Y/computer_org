aa6395632ac9ea3fb74ac5c835ada3e73cbc2776
Oliver-Y <31872294+Oliver-Y@users.noreply.github.com>
2019-09-26 23:47:16 -0400

    untabify fix

--- untabify.c
+++ untabify.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-  char *string;
   char *newstring;
   int counter = 0;
   int *spaces_needed;
   int offset = 0;
-  spaces_needed = calloc(15, sizeof(int));
-  string = malloc(300 * sizeof(char));
-  newstring = malloc(300*sizeof(char));
-  fgets(string,300,stdin);
-  while (*string != 10){
-    if(*string == 9){
+  spaces_needed = calloc(100, sizeof(int));
+  newstring = calloc(500,sizeof(char));
+  char a;
+  while ((a = fgetc(stdin)) != EOF){
+    if (a == 9){
       for (int i = 0; i < 8-(counter%8); i++){
         *newstring = ' ';
         newstring++;
         offset++;
       }
     }
-      *newstring = *string;
+    else{
+      *newstring = a;
       newstring++;
-      string++;
       counter++;
-  }
-  newstring -= counter + offset;
-  for (int i = 0; i < counter + offset; ++i){
-    if(*newstring != 9){
-      printf("%c",*newstring);
     }
-    newstring++;
   }
-  return 1;
+  newstring -= counter+offset;
+  printf("%s",newstring);
 }

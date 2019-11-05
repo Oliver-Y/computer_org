7b74cbcb259b37c45a87d31653a706651fc2a4cb
Oliver-Y <31872294+Oliver-Y@users.noreply.github.com>
2019-11-05 10:47:33 -0500

    theseus

--- theseus/chase.c
+++ theseus/chase.c
@@ -26,17 +26,44 @@ int chase(level *l, int s)
 
 int moveM(level *l)
 {
+  int minotaur_x;
+  int minotaur_y;
   int player_x;
   int player_y;
-  p2rc(l->minotaur,&player_x,&player_y);
-  //Check to see if X position is movable
-  int counter = 0;
-/*  if (counter == 0){
-    (l->minotaur) |
-  }*/
-  printf("this is x pos: %d ", player_x);
-  printf("this is y pos: %d ", player_y);
-  movePiece(l,MINOTAUR,0,1,0,2);
-
-  return 0;
+  int dr = 0;
+  int dc = 0;
+  int moved = 0;
+  int sch;
+  p2rc(l->minotaur,&minotaur_x,&minotaur_y);
+  p2rc(l->theseus,&player_x,&player_y);
+  sch = get(l,minotaur_x,minotaur_y);
+  //Figure out the direction
+  // if (mintaur_y < player_y) dc = 1; else dc = -1
+  // if minotaur_y == player_y) dc = 0;
+  // if (minotaurx < playerx) dr = 1, dr = -1
+  // if minotaur_x == player_x dr = 0
+  dc = minotaur_y < player_y ? 1:-1;
+  if(minotaur_y == player_y) dc = 0;
+  dr = minotaur_x < player_x ? 1:-1;
+  if(minotaur_x == player_x) dr = 0;
+  if(!(sch & LEFT) && dc == -1){
+    movePiece(l,MINOTAUR,minotaur_x,minotaur_y,minotaur_x,minotaur_y+dc);
+    moved = 1;
+  }
+  else if(!(sch & RIGHT) && dc == 1){
+    movePiece(l,MINOTAUR,minotaur_x,minotaur_y,minotaur_x,minotaur_y+dc);
+    moved = 1;
+  }
+  else if(!(sch & DOWN) && dr == 1){
+    movePiece(l,MINOTAUR,minotaur_x,minotaur_y,minotaur_x+dr,minotaur_y);
+    moved = 1;
+  }
+  else if(!(sch & UP) && dr == -1){
+    movePiece(l,MINOTAUR,minotaur_x,minotaur_y,minotaur_x+dr,minotaur_y);
+    moved = 1;
+  }
+  else{
+    moved = 0;
+  }
+  return moved;
 }
